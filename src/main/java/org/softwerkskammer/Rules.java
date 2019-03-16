054903065df61792917ec337645e899e0d0a6930
Adam Zielinski <adam.zielinski@kurier.at>
2019-03-16 11:12:46 +0100

    Simpler rules

--- src/main/java/org/softwerkskammer/Rules.java
+++ src/main/java/org/softwerkskammer/Rules.java
@@ -6,22 +6,14 @@ public class Rules {
     public static final boolean DEAD = false;
 
     public boolean next(boolean isAlive, int livingNeighbours) {
-        // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-        if (isAlive && livingNeighbours < 2) {
-            return false;
-        }
-        // Any live cell with two or three live neighbours lives on to the next generation.
-        if (isAlive && (livingNeighbours == 2 || livingNeighbours == 3)) {
-            return true;
-        }
-        // Any live cell with more than three live neighbours dies, as if by overpopulation.
-        if (isAlive && livingNeighbours > 3) {
-            return false;
+        // Any live cell with ...
+        if (isAlive) {
+            // ...fewer than two live neighbours dies, as if by underpopulation.
+            // ...two or three live neighbours lives on to the next generation.
+            // ...more than three live neighbours dies, as if by overpopulation.
+            return (livingNeighbours == 2 || livingNeighbours == 3);
         }
         // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-        if (!isAlive && livingNeighbours == 3) {
-            return true;
-        }
-        return isAlive;
+        return livingNeighbours == 3;
     }
 }
