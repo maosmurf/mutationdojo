f7ff90d946b2632aa224156217383f79874e61cb
Adam Zielinski <adam.zielinski@kurier.at>
2019-03-16 11:12:27 +0100

    Parameterized junit

--- src/test/java/org/softwerkskammer/RulesTest.java
+++ src/test/java/org/softwerkskammer/RulesTest.java
@@ -1,46 +1,64 @@
 package org.softwerkskammer;
 
-import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.softwerkskammer.Rules.ALIVE;
 import static org.softwerkskammer.Rules.DEAD;
 
+@RunWith(Parameterized.class)
 public class RulesTest {
 
     private Rules rules;
 
-    @Before
-    public void init() {
+    private boolean isAlive;
+    private int livingNeighbours;
+    private boolean expected;
+
+    public RulesTest(boolean isAlive, int livingNeighbours, boolean expected) {
         this.rules = new Rules();
+
+        this.isAlive = isAlive;
+        this.livingNeighbours = livingNeighbours;
+        this.expected = expected;
     }
 
-    @org.junit.Test
-    public void diesOfLonliness() {
-        assertThat(this.rules.next(ALIVE, 1), is(DEAD));
+    @Test
+    public void willSurvive() {
+        assertThat(this.rules.next(isAlive, livingNeighbours), is(expected));
     }
 
-    @org.junit.Test
-    public void twoSurvives() {
-        assertThat(this.rules.next(ALIVE, 2), is(ALIVE));
-    }
+    @Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = {
+                {DEAD, 0, DEAD},
+                {DEAD, 1, DEAD},
+                {DEAD, 2, DEAD},
+                {DEAD, 3, ALIVE},
+                {DEAD, 4, DEAD},
+                {DEAD, 5, DEAD},
+                {DEAD, 6, DEAD},
+                {DEAD, 7, DEAD},
+                {DEAD, 8, DEAD},
+                {ALIVE, 0, DEAD},
+                {ALIVE, 1, DEAD},
+                {ALIVE, 2, ALIVE},
+                {ALIVE, 3, ALIVE},
+                {ALIVE, 4, DEAD},
+                {ALIVE, 5, DEAD},
+                {ALIVE, 6, DEAD},
+                {ALIVE, 7, DEAD},
+                {ALIVE, 8, DEAD},
+        };
 
-    // todo will mutation find 3?
-
-    @org.junit.Test
-    public void diesOfOverpopulation() {
-        assertThat(this.rules.next(ALIVE, 4), is(DEAD));
-    }
-
-    @org.junit.Test
-    public void deadWithThreeWillResurrect() {
-        assertThat(this.rules.next(DEAD, 3), is(ALIVE));
-    }
-
-    @org.junit.Test
-    public void deadWithFourWillResurrect() {
-        assertThat(this.rules.next(DEAD, 4), is(DEAD));
+        return Arrays.asList(data);
     }
 
 }
